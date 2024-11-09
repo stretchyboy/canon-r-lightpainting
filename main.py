057fa54b4fa98f3ccd03835d9395b02d25b0d311
Martyn Eggleton <martyn.eggleton@gmail.com>
2024-11-09 16:06:51 +0000

    Catch more errors

--- main.py
+++ main.py
@@ -17,16 +17,10 @@ import plasma
 from plasma import plasma_stick
 import time
 from stickframeplayer import StickFramePlayer
-
+import random
 
 # Set how many LEDs you have
 NUM_LEDS = 144
-
-# The SPEED that the LEDs cycle at (1 - 255)
-SPEED = 20
-
-# How many times the LEDs will be updated per second
-UPDATES = 40
 
 # WS2812 / NeoPixel™ LEDs
 led_strip = plasma.WS2812(NUM_LEDS, 0, 0, plasma_stick.DAT, color_order=plasma.COLOR_ORDER_GRB)
@@ -35,25 +29,15 @@ led_strip = plasma.WS2812(NUM_LEDS, 0, 0, plasma_stick.DAT, color_order=plasma.C
 led_strip.start()
 led_strip.clear()
 
-
-
 APIURL = "https://raw.githubusercontent.com/stretchyboy/lp-projects/main/"
-
-ip = connect_to_wifi(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
+ip = None
+while ip == None :
+    ip = connect_to_wifi(secrets.WIFI_SSID, secrets.WIFI_PASSWORD, 30)
 info("ip", ip)
 CAMERAURL = f"http://{secrets.CAMERAIP}:8080/ccapi"
 
 cache = {}
-
        
-# TODO : show list of images from the server
-'''
-async def stickframe_categories(categories):
-    cats = [render_template("stickframe-category", name=name) for name, items in categories.items()]    
-    out = "\n".join(cats)
-    return out
-'''
-
 def stickframe_categories(categories):
     out = ""
     for catname, anim in categories.items():
@@ -67,7 +51,7 @@ def stickframe_categories(categories):
 
 async def fetchCached(base, fetchtype, path):
     if fetchtype in cache:
-        if cache[fetchtype]['path'] == path:
+        if "path" in cache[fetchtype] and cache[fetchtype]['path'] == path:
             info(">", 'Cache Used for', fetchtype, path)
             return cache[fetchtype]['data']
     cache[fetchtype] = {}
@@ -119,39 +103,52 @@ def render_options(items, selected=0):
     return out        
 
 @server.route("/categories/<cat>", methods=["GET"])
-def index(request, cat):
-    j = await fetchCached(APIURL, "category", f"data/categories/{cat}.json")    
-    cat = server.urldecode(cat)
-    
-    return await render_template("list.html", name=f"Category: {cat}", path=f"/categories/{cat}/anim/", items=j)
-    
+def categories_index(request, cat):
+    try:
+        j = await fetchCached(APIURL, "category", f"data/categories/{cat}.json")    
+        cat = server.urldecode(cat)    
+        return await render_template("list.html", name=f"Category: {cat}", path=f"/categories/{cat}/anim/", items=j)
+    except Exception as e:
+        error("> categories/index Exception ", e)
+        led_strip.clear()
+        return server.Response('{"success":0}', status=500)#, headers={"Content-Type":"application/json"})
+        
 @server.route("/categories/<cat>/anim/<anim>", methods=["GET"])
 def control(request, cat, anim):
-    k = fetchCached(APIURL, "frame", f"data/categories/{cat}/anim/{anim}/1.json")
-    j = await fetchCached(APIURL, "anim", f"data/categories/{cat}/anim/{anim}.json")
-    l = await k
-    #print("l", l)
-    sWidthCM = str(l["widthCM"])
+    try:
+        k = fetchCached(APIURL, "frame", f"data/categories/{cat}/anim/{anim}/1.json")
+        j = await fetchCached(APIURL, "anim", f"data/categories/{cat}/anim/{anim}.json")
+        l = await k
+        #print("l", l)
+        sWidthCM = str(l["widthCM"])
+        
+        print("sWidthCM", sWidthCM)
+        cat = server.urldecode(cat)
+        anim = server.urldecode(anim)
+        src = ""
+        if "http" in j['source']:
+            src = j['source']
+        return await render_template(
+            "control.html",
+            name=f"Category: {cat} : Anim: {anim}",
+            path=f"/categories/{cat}/anim/",
+            cat=cat,
+            anim=anim,
+            currentframe = "1",
+            framecount=str(j['frames']),
+            src=src,
+            widthCM = sWidthCM,
+            CAMERAURL = CAMERAURL 
+        )
+    except Exception as e:
+        error("> control Exception ", e)
+        led_strip.clear()
+        return await render_template(
+            "500.html",
+            name=f"Category: {cat} : Anim: {anim}",
+            status=500)
+        #return server.Response('{"success":0}', status=500)
     
-    print("sWidthCM", sWidthCM)
-    cat = server.urldecode(cat)
-    anim = server.urldecode(anim)
-    src = ""
-    if "http" in j['source']:
-        src = j['source']
-    return await render_template(
-        "control.html",
-        name=f"Category: {cat} : Anim: {anim}",
-        path=f"/categories/{cat}/anim/",
-        cat=cat,
-        anim=anim,
-        currentframe = "1",
-        framecount=str(j['frames']),
-        src=src,
-        widthCM = sWidthCM,
-        CAMERAURL = CAMERAURL 
-)
-
 @server.route("/js/<filename>", ["GET"])
 def staticjs(request, filename):
     with open(filename, "r") as f:
@@ -188,35 +185,111 @@ def load_frame(request, cat, anim, frame):
         #return server.Response(text, status=200, headers={"Content-Type":"application/json"})
     except ValueError as e:
         error("> load_frame ValueError ", e)
+        return server.Response('{"success":0}', status=500)
     except MemoryError as e:
         error("> load_frame MemoryError ", e)
+        return server.Response('{"success":0}', status=500)
     except Exception as e:
         error("> load_frame Exception ", e)
-        
+        return server.Response('{"success":0}', status=500)
+
+def hex_to_rgb(value):
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+def makeRandomLines(width, height):
+    aLines = []
+    for y in range(height):
+        line = [random.randint(0, width), random.randint(0, width)]
+        line.sort()
+        aLines.append(line)
+    return aLines
+
 @server.route("/show/<cat>/anim/<anim>/<frame>/<duration>", methods=["GET"])
 def show_frame(request, cat, anim, frame, duration):
     try:
+        x=0
+        hideWhite = request.query.get("hidewhite", "off") == "on"
+        paintBlack = request.query.get("paintblack", "off") == "on"
+        paintBlackAs = request.query.get("paintblackas", "FFFFFF")
+        bSpeckles = request.query.get("speckles", "off") == "on"
+        bLines = request.query.get("lines", "off") == "on"
+        
+        
+        #print("hideWhite",hideWhite, "paintBlack", paintBlack, "paintBlackAs", paintBlackAs)
+        paintBlackAsColour = list(hex_to_rgb(paintBlackAs))
+        #print("paintBlackAsColour", paintBlackAsColour)
         j = await fetchCached(APIURL, "frame", f"data/categories/{cat}/anim/{anim}/{frame}.json")
         #print("show_frame", j)
         ministick = StickFramePlayer()
         ministick.loadJson(j)
-        rowdur = float(duration)/float(ministick.width)
-        try : 
+        rowdur = int(1000000000 * float(duration)/float(ministick.width))
+        if(bLines):
+            aLines = makeRandomLines(ministick.width, ministick.height)
+        
+        startTime = time.time_ns()
+        nowTime = startTime
+        targetTime = startTime + rowdur
+        #print("duration", duration,"ministick.width", ministick.width, "rowdur", rowdur, "startTime",startTime,"targetTime",targetTime )
+        palette = []
+        for i in range(0, len(ministick.ourPalette), 3):
+          r = ministick.ourPalette[i]
+          g = ministick.ourPalette[i+1]
+          b = ministick.ourPalette[i+2]
+          palette.append(r)
+          palette.append(g)
+          palette.append(b)
+          #print("i", i,r,g,b)
+          if hideWhite and r>253 and g>253 and b>253:
+            palette[i]   = 0
+            palette[i+1] = 0
+            palette[i+2] = 0
+          else:  
+            if paintBlack:
+              palette[i]   = int(paintBlackAsColour[0] * (255 - r ) / 255 )
+              palette[i+1] = int(paintBlackAsColour[1] * (255 - g ) / 255 )
+              palette[i+2] = int(paintBlackAsColour[2] * (255 - b ) / 255 )
+        
+        try :
             for col in ministick.getNextColumn():
               for y in range(ministick.height):
+                r=0
+                g=0
+                b=0
                 try :
-                    r = ministick.ourPalette[col[y]*3]
-                    g = ministick.ourPalette[(col[y]*3)+1]
-                    b = ministick.ourPalette[(col[y]*3)+2]
+                    r = palette[col[y]*3]
+                    g = palette[(col[y]*3)+1]
+                    b = palette[(col[y]*3)+2]
                 except Exception as e:
                     pass
                     #error("> show_frame within palette Exception ", e, col[y]*3, len(ministick.ourPalette))
                 try:
-                    led_strip.set_rgb(y, r, g, b, 10 )
+                    level = 1
+                    if bLines:
+                        if(x >= aLines[y][0] and x <= aLines[y][1]):
+                            if(bSpeckles):
+                                level = random.random()
+                            else:
+                                level = 1
+                        else:
+                            level = 0
+                    elif bSpeckles :
+                        level = random.random()
+                    if level < 1:
+                        r = int(level * r)
+                        g = int(level * g)
+                        b = int(level * b)
+                        
+                    led_strip.set_rgb(y+(144 - ministick.height), r, g, b, 10 )
                 except Exception as e:
                     error("> show_frame set_rgb Exception ", e, y, r, g, b, 10 )
-                
-              time.sleep(rowdur)
+              x += 1  
+              while nowTime < targetTime:
+                  time.sleep_ms(0)
+                  nowTime = time.time_ns()
+              targetTime += rowdur 
+              #time.sleep(rowdur)
         except Exception as e:
             error("> show_frame within col Exception ", e, col)
         led_strip.clear()
@@ -236,20 +309,28 @@ def show_frame(request, cat, anim, frame, duration):
     
 @server.route("/sightingdots/<on>", methods=["GET"]) 
 def showSightingDots(request, on):
-    if int(on):
-        led_strip.set_rgb(0,255,255,255,10)
-        led_strip.set_rgb(NUM_LEDS-1,255,255,255,10)
-    else:
+    try:
+        if int(on):
+            led_strip.set_rgb(0,255,0,0,10) #top
+            led_strip.set_rgb(NUM_LEDS-1,0,0,255,10) #top
+        else:
+            led_strip.clear()
+        return server.Response('{"success":1}', status=200)#, headers={"Content-Type":"application/json"})
+    except Exception as e:
+        error("> showSightingDots Exception ", e)
         led_strip.clear()
-    return server.Response('{"success":1}', status=200)#, headers={"Content-Type":"application/json"})
-    
+        return server.Response('{"success":0}', status=500)#, headers={"Content-Type":"application/json"})
+        
 @server.route("/", methods=["GET"])
 def index(request):
-    j = await fetchCached(APIURL, "catergories", "data/categories.json")
-
-    return await render_template("list.html", name="Catergories", path="/categories/", items=j)
+    try:
+        j = await fetchCached(APIURL, "catergories", "data/categories.json")
+        return await render_template("list.html", name="Catergories", path="/categories/", items=j)
+    except Exception as e:
+        error("> index Exception ", e)
+        led_strip.clear()
+        return server.Response('{"success":0}', status=500)#, headers={"Content-Type":"application/json"})
     
-
 
 # catchall example
 @server.catchall()
